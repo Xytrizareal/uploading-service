e41ee498e5855a6106c9ae806557c8bfa26f75db
Xytrizareal <cryfxreal@gmail.com>
2024-03-22 07:57:11 -0700

    added a option for custom chars in generate string which will be used later

--- incl/mainLib.php
+++ incl/mainLib.php
@@ -33,9 +33,8 @@ class mainLib {
                 $stmt->close();
             }
         }
-    }    
-    public function generateRandomString($length) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    public function generateRandomString($length, $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
             $randomString .= $characters[random_int(0, strlen($characters) - 1)];
